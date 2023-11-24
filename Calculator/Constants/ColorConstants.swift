fd19bba8bf3cf9334addefa4cbc14115871ce6a8
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2023-11-24 20:41:40 +0300

    feat: init colours to that project

---
+++ Calculator/Constants/ColorConstants.swift
@@ -0,0 +1,31 @@
+//
+//  ColorConstants.swift
+//  Calculator
+//
+//  Created by MIKHAIL ZHACHKO on 24.11.23.
+//
+
+import SwiftUI
+
+let backGroundColor1: Color = Color("backGroundColor1")
+let backGroundColor2: Color = Color("backGroundColor2")
+let buttonTextColor: Color = Color("buttonTextColor")
+
+
+let numberButtonColor1: Color = Color("numberButtonColor1")
+let numberButtonColor2: Color = Color("numberButtonColor2")
+let numbersBGColor: Color = Color("numbersBGColor")
+
+let orangeBGButtonColor: Color = Color("orangeBGButtonColor")
+let orangeButtonColor1: Color = Color("orangeButtonColor1")
+let orangeButtonColor2: Color = Color("orangeButtonColor2")
+let orangeButtonTextColor: Color = Color("orangeButtonTextColor")
+
+let expressionsTextColor: Color = Color("expressionsTextColor")
+let resultTextColor: Color = Color("resultTextColor")
+
+let topRowBGButtonColor: Color = Color("topRowBGButtonColor")
+let topRowButtonColor1: Color = Color("topRowButtonColor1")
+let topRowButtonColor2: Color = Color("topRowButtonColor2")
+
+
